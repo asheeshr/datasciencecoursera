38dc03100ce6f88f1ab8872b63821c52ce5eacc5
Asheesh <ashrj@outlook.com>
2014-05-15 18:47:51 +0530

    First assignment completed

--- Prog-Assignment-1/code/complete.R
+++ Prog-Assignment-1/code/complete.R
@@ -12,4 +12,17 @@ complete <- function(directory, id = 1:332) {
     ## ...
     ## where 'id' is the monitor ID number and 'nobs' is the
     ## number of complete cases
+    nobs <- vector(mode="numeric", length=0)
+    
+    for(i in id)
+    {
+        #Add data from CSV file in data frame
+        df <- read.csv(paste("../", directory, "/", sprintf("%03d",i) , ".csv", sep=""))
+        #l1 <- !is.na(df$nitrate)
+        #l2 <- !is.na(df$sulfate)
+        nobs <- c( nobs, sum(!is.na(df$nitrate) & !is.na(df$sulfate)))    
+        #nobs <- c( nobs, sum(l1 & l2))    
+    }
+    
+    data.frame(id, nobs)
 }
--- Prog-Assignment-1/code/corr.R
+++ Prog-Assignment-1/code/corr.R
@@ -6,6 +6,28 @@ corr <- function(directory, threshold = 0) {
     ## number of completely observed observations (on all
     ## variables) required to compute the correlation between
     ## nitrate and sulfate; the default is 0
+    options(digits=4)
+    
+    nobs <- vector(mode="numeric", length=0)
+    corvec <-  vector(mode="numeric", length=0)
+    
+    for(i in 1:332)
+    {
+        #Add data from CSV file in data frame
+        df <- read.csv(paste("../", directory, "/", sprintf("%03d",i) , ".csv", sep=""))
+        l1 <- !is.na(df$nitrate)
+        l2 <- !is.na(df$sulfate)
+        #print(paste("./", directory, "/", sprintf("%03d",i) , ".csv", sep=""))
+        if(sum(l1 & l2) > threshold)    
+        {
+            corvec <- c(corvec, cor(df$nitrate, df$sulfate, use="complete.obs", method="pearson"))
+                                    #x=df[complete.cases(df[sulfate, nitrate]), sulfate], 
+                                    #y=df[complete.cases(df[sulfate, nitrate]), nitrate],
+                                    #use="everything")) #pairwise.complete.obs"))
+        }
+        
+    }
     
     ## Return a numeric vector of correlations
+    corvec
 }
